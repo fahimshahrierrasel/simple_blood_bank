e9202f9189a79f3bc8b00a7e5756df36ccae47e2
Md. Fahim Shahrier Rasel <fahimshahrier2@gmail.com>
2017-10-03 15:35:09 +0600

    Database connection with Configuration manager

---
+++ Blood Bank Management/DBConnection.cs
@@ -0,0 +1,29 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Blood_Bank_Management
+{
+    class DbConnection
+    {
+        private SqlConnection connection;
+        private SqlCommand command;
+
+        public SqlConnection Connection
+        {
+            get { return connection; }
+        }
+
+        public SqlCommand Command
+        {
+            get { return command; }
+        }
+
+        public DbConnection()
+        {
+            connection = new SqlConnection();
+            command = new SqlCommand();
+            connection.ConnectionString = ConfigurationManager.ConnectionStrings["BBConnectionString"].ConnectionString;
+            command.Connection = connection;
+        }
+    }
+}
