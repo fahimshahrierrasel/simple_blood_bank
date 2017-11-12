b9fe854064f84f7091e2e25b9a2406281cb12cba
Md. Fahim Shahrier Rasel <fahimshahrier2@gmail.com>
2017-11-12 22:39:06 +0600

    nUnit unit test add as a project

---
+++ Blood Bank Management.nUnit/UserRegistrationTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Blood_Bank_Management.nUnit
+{
+    [TestFixture]
+    public class UserRegistrationTests
+    {
+        [TestCase]
+        public void HasEnoughDateToDonateTest()
+        {
+            // arrange
+            var userController = new UserController();
+            var dob = new DateTime(1990, 1, 1);
+            // act
+            var hasAge = userController.HasEnoughAgeToDonate(dob);
+            // assert
+            Assert.AreEqual(true, hasAge);
+        }
+
+        [TestCase]
+        public void ValidateRegistrationDataTest()
+        {
+            // arrange
+            var userController = new UserController();
+            var userName = "Fahim Shahrier Rasel";
+            var dob = new DateTime(1990, 1, 1);
+            var weight = 50;
+            var mobileNumber = "01554070646";
+            var address = "Middle Badda";
+
+            // act
+            var isValid = userController.ValidateRegistrationData(userName, dob, weight, mobileNumber, address);
+            // assert
+            Assert.AreEqual(true, isValid);
+        }
+    }
+}
