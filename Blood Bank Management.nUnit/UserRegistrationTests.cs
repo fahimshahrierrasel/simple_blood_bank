4edf07fc72265af9d9507c4b530fb67594be21cc
Md. Fahim Shahrier Rasel <fahimshahrier2@gmail.com>
2017-12-12 14:54:14 +0600

    Some new unit test add to UserRegistrationTests

--- Blood Bank Management.nUnit/UserRegistrationTests.cs
+++ Blood Bank Management.nUnit/UserRegistrationTests.cs
@@ -72,6 +72,70 @@ namespace Blood_Bank_Management.nUnit
             Assert.Throws<NullReferenceException>(() =>
                 userController.ValidateRegistrationData(userName, dob, weight, mobileNumber, address));
         }
-        
+
+        [TestCase]
+        public void ValidateNotEnoughAgeRegistrationDataTest()
+        {
+            // arrange
+            var userController = new UserController();
+            string userName = "Fahim Shahrier Rasel";
+            var dob = new DateTime(2010, 1, 1);
+            var weight = 50;
+            var mobileNumber = "01554070646";
+            var address = "Middle Badda";
+            // act
+            var isValid = userController.ValidateRegistrationData(userName, dob, weight, mobileNumber, address);
+            // assert
+            Assert.AreEqual(false, isValid);
+        }
+
+        [TestCase]
+        public void ValidateUnderWeightRegistrationDataTest()
+        {
+            // arrange
+            var userController = new UserController();
+            string userName = "Fahim Shahrier Rasel";
+            var dob = new DateTime(1990, 1, 1);
+            var weight = 46;
+            var mobileNumber = "01554070646";
+            var address = "Middle Badda";
+            // act
+            var isValid = userController.ValidateRegistrationData(userName, dob, weight, mobileNumber, address);
+            // assert
+            Assert.AreEqual(false, isValid);
+        }
+
+        [TestCase]
+        public void ValidateIncorrectMobileNumberRegistrationDataTest()
+        {
+            // arrange
+            var userController = new UserController();
+            string userName = "Fahim Shahrier Rasel";
+            var dob = new DateTime(1990, 1, 1);
+            var weight = 50;
+            var mobileNumber = "015540BBSA70646";
+            var address = "Middle Badda";
+            // act
+            var isValid = userController.ValidateRegistrationData(userName, dob, weight, mobileNumber, address);
+            // assert
+            Assert.AreEqual(false, isValid);
+        }
+
+        [TestCase]
+        public void ValidateNullAddressRegistrationDataTest()
+        {
+            // arrange
+            var userController = new UserController();
+            string userName = "Fahim Shahrier Rasel";
+            var dob = new DateTime(1990, 1, 1);
+            var weight = 50;
+            var mobileNumber = "01554070646";
+            string address = null;
+
+            // assert
+            Assert.Throws<NullReferenceException>(() =>
+                userController.ValidateRegistrationData(userName, dob, weight, mobileNumber, address));
+        }
+
     }
 }
